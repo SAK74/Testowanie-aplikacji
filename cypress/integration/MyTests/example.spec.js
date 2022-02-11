aa116ca91a5b119ffd79e86de6a3a36c99a5949a
SAK74 <sergijks@gmail.com>
2022-02-11 19:16:27 +0100

    change external URL & format code

--- cypress/integration/MyTests/example.spec.js
+++ cypress/integration/MyTests/example.spec.js
@@ -13,24 +13,24 @@ describe('Cypress testing', () => {
             .should('contain', '+')
             .should('contain', '-');
     });
-    it ('should counter be modified after cliked +/-', () => {
+    it('should counter be modified after cliked +/-', () => {
         const increment = cy.get('@buttons').contains('+');
-        for (let i = 0; i < 5; i++){
+        for (let i = 0; i < 5; i++) {
             increment.click();
         }
         cy.get('@counter').should('have.text', '27');
         cy.get('@buttons').contains('-').click();
         cy.get('@counter').should('have.text', '26');
     });
-    it ('should counter be modified after clicked "Zmień"', () => {
+    it('should counter be modified after clicked "Zmień"', () => {
         const TESTVALUE = '367';
-        cy.get('input').type(TESTVALUE, {delay: 500}).should('have.value', TESTVALUE);
+        cy.get('input').type(TESTVALUE, { delay: 500 }).should('have.value', TESTVALUE);
         cy.wait(500);
         cy.get('@buttons').contains('Zmień').click();
         cy.get('@counter').should('have.text', TESTVALUE);
         cy.wait(500);
     });
-    it ('should counter be {start} after clicked "Reset"', () => {
+    it('should counter be {start} after clicked "Reset"', () => {
         cy.get('@buttons').contains('Reset').click();
         cy.get('@counter').should('have.text', '22');
     })
--- cypress/integration/MyTests/internal.spec.js
+++ cypress/integration/MyTests/internal.spec.js
@@ -1,13 +1,13 @@
 describe('internal site testing', () => {
     beforeEach(() => {
-        cy.visit('https://sak74.github.io/');
+        cy.visit('https://flamboyant-euclid-991866.netlify.app/');
     });
-    it ('field should be "warning" without value, "Ok" disabled', () => {
+    it('field should be "warning" without value, "Ok" disabled', () => {
         cy.get('input').each(el => el.focus().blur());
         cy.contains('button', 'Ok').should('be.disabled');
         cy.pause();
     });
-    it ('"Ok" should be active after typed all field', () => {
+    it('"Ok" should be active after typed all field', () => {
         cy.get('input[name=preparation_time]').type('05:00:00');
         cy.get('input[name=type]').parent().click('right');
         cy.get('li').first().click();
